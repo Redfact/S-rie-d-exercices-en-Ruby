54b93d021259c8119b2eb69fb73baba8d02277b0
Redfact <tantelyrandria@ymail.com>
2020-04-07 12:40:20 +0000

    Ajout commentaires exo5

---
+++ exo_05.rb
@@ -0,0 +1,33 @@
+puts "On va compter le nombre d'heures de travail à THP"
+#  #{} evalue et renvoye la valeur de l'operation entre {}
+
+#calcul nombre d'heure de travaille
+puts "Travail : #{10 * 5 * 11}"
+
+#convertit le nombre d'heure minutes
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+
+puts "Et en secondes ?"
+
+#puis en secondes
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#fait une comparaison entre différentes valeurs
+#renvoye vrai ou faux
+puts 3 + 2 < 5 - 7
+
+#Evaluation des différentes types d'opération(calcul,comparaison)
+#avec #{}
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
